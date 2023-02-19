55127ec77ce3179a72ab3e37cf20ed2022d37660
sebastiannesheim <sebnes02@gmail.com>
2023-02-19 23:00:37 +0100

    Skrev inn koden

--- main.go
+++ main.go
@@ -7,8 +7,11 @@ import (
 
 // Definerer flag-variablene i hoved-"scope"
 var fahr float64
+var celsius float64
+var kelvin float64
 var out string
 var funfacts string
+var temperatureScale string
 
 // Bruker init (som anbefalt i dokumentasjonen) for å sikre at flagvariablene
 // er initialisert.
@@ -23,17 +26,39 @@ func init() {
 
 	// Definerer og initialiserer flagg-variablene
 	flag.Float64Var(&fahr, "F", 0.0, "temperatur i grader fahrenheit")
+	flag.Float64Var(&celsius, "C", 0.0, "temperature in degrees celsius")
+	flag.Float64Var(&kelvin, "K", 0.0, "temperature in degrees kelvin")
 	// Du må selv definere flag-variablene for "C" og "K"
 	flag.StringVar(&out, "out", "C", "beregne temperatur i C - celsius, F - farhenheit, K- Kelvin")
 	flag.StringVar(&funfacts, "funfacts", "sun", "\"fun-facts\" om sun - Solen, luna - Månen og terra - Jorden")
 	// Du må selv definere flag-variabelen for -t flagget, som bestemmer
 	// hvilken temperaturskala skal brukes når funfacts skal vises
 
+	flag.StringVar(&temperatureScale, "t", "C", "temperaturskala som skal brukes når fun-facts vises")
+
 }
 
 func main() {
 
 	flag.Parse()
+
+	// Check for conflicting flags
+	if isFlagPassed("F") && isFlagPassed("C") {
+		flagError("-F", "-C")
+		return
+	} else if isFlagPassed("F") && isFlagPassed("K") {
+		flagError("-F", "-K")
+		return
+	} else if isFlagPassed("C") && isFlagPassed("K") {
+		flagError("-C", "-K")
+		return
+	} else if isFlagPassed("funfacts") && (isFlagPassed("F") || isFlagPassed("C") || isFlagPassed("K")) {
+		fmt.Println("Error: -funfacts cannot be used with -F, -C, or -K flags")
+		return
+	} else if isFlagPassed("funfacts") && !isFlagPassed("t") {
+		fmt.Println("Error: -funfacts must be used with -t flag")
+		return
+	}
 
 	/**
 	    Her må logikken for flaggene og kall til funksjoner fra conv og funfacts
@@ -57,6 +82,31 @@ func main() {
 	    beskrivelsene av flagg-variablene, som angitt i parameter fire til
 	    funksjonene Float64Var og StringVar
 	*/
+
+	if isFlagPassed("F") && isFlagPassed("out") {
+		result := FahrenheitToCelsius(fahr)
+		fmt.Printf("%0.2f°F is %0.2f°C\n", fahr, result)
+	} else if isFlagPassed("C") && isFlagPassed("out") {
+		result := CelsiusToFahrenheit(celsius)
+		fmt.Printf("%0.2f°C is %0.2f°F\n", celsius, result)
+	} else if isFlagPassed("K") && isFlagPassed("out") {
+		result := KelvinToCelsius(kelvin)
+		fmt.Printf("%0.2f°K is %0.2f°C\n", kelvin, result)
+	} else if isFlagPassed("C") && isFlagPassed("fact") {
+		result := funfacts.RandomCelsiusFact()
+		fmt.Println(result)
+	} else if isFlagPassed("F") && isFlagPassed("fact") {
+		result := funfacts.RandomFahrenheitFact()
+		fmt.Println(result)
+	} else if isFlagPassed("K") && isFlagPassed("fact") {
+		result := funfacts.RandomKelvinFact()
+		fmt.Println(result)
+	} else if isFlagPassed("conv") {
+		result := conv.Convert(value, unit)
+		fmt.Printf("%0.2f %s is %0.2f %s\n", value, unit, result.Value, result.Unit)
+	} else {
+		flag.Usage()
+	}
 
 	// Her er noen eksempler du kan bruke i den manuelle testingen
 	fmt.Println(fahr, out, funfacts)
@@ -85,4 +135,9 @@ func isFlagPassed(name string) bool {
 		}
 	})
 	return found
+}
+
+func flagError(flag1, flag2 string) {
+	fmt.Printf("Error: %s and %s flags cannot be used simultaneously\n", flag1, flag2)
+	return
 }
