074f208bc6088d12663e6a8531094e0d7314a9d1
sebastiannesheim <sebnes02@gmail.com>
2023-02-23 03:12:18 +0100

    små endringer

--- conv/conv.go
+++ conv/conv.go
@@ -1,26 +1,26 @@
 package conv
 
 func KelvinToCelsius(value float64) float64 {
-	return (value + 273.15)
+	return value - 273.15
 }
 
 func CelsiusToKelvin(value float64) float64 {
-	return (value - 273.15)
+	return value + 273.15
 }
 
 func CelsiusToFahrenheit(value float64) float64 {
-	return (value*(9/5) + 32)
+	return value*9/5 + 32
 }
 
 func FahrenheitToCelsius(value float64) float64 {
-	return (value - 32) * (5 / 9)
+	return (value - 32) * 5 / 9
 }
 
 func KelvinToFahrenheit(value float64) float64 {
-	return (value-273.15)*(9/5) + 32
+	return (value-273.15)*9/5 + 32
 }
 
 
 func FahrenheitToKelvin(value float64) float64 {
-	return (value-32)*(5/9) + 273.15
+	return (value-32)*5/9 + 273.15
 }
